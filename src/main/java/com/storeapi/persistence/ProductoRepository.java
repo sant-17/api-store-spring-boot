ed59726217212c2ce832696cc6861883b765bfb3
Santiago <santiago.garciah17@gmail.com>
2023-03-01 12:04:49 -0500

    Creating getProducto, save and delete on ProductoRepository

--- src/main/java/com/storeapi/persistence/ProductoRepository.java
+++ src/main/java/com/storeapi/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.storeapi.persistence;
 
 import com.storeapi.persistence.crud.ProductoCrudRepository;
 import com.storeapi.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -20,5 +22,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
